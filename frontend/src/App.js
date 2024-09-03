57964325d6d44e01c73668e98f028ecce88a53c9
Orijadribo <danielorijadribo@gmail.com>
2024-09-04 02:34:33 +0300

    NavBar and created other pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
+import Home from './pages/Home';
+import About from './pages/About';
+import Properties from './pages/Properties';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';    
+
+
 function App() {
   return (
-    <div className='font-urbanist'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
-    </div>
+    <Router>
+      <div className='font-urbanist'>
+        <NavBar />
+        <Routes>
+          <Route index path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/properties' element={<Properties />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrolled]);
+
+  return (
+    <div className={`fixed w-full transition-colors duration-300 ${scrolled ? 'bg-white' : 'bg-transparent'}`}>
+      <div className='flex justify-between items-center max-w-screen-xl mx-auto h-16'>
+        <div className='text-2xl font-bold'>Logo</div>
+        <div className='flex justify-between items-center gap-10'>
+          <div className='flex items-center justify-center space-x-4'>
+            <Link to='/'>
+              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>Home</div>
+            </Link>
+            <Link to='/about'>
+              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>About Us</div>
+            </Link>
+            <Link to='/properties'>
+              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>Properties</div>
+            </Link>
+            <Link to='/contact'>
+              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>Contact Us</div>
+            </Link>
+          </div>
+          <div className='flex items-center justify-center space-x-4'>
+            <Link to='/login'>
+              <div className='border-2 border-white hover:border-2 hover:border-slate-500 px-4 py-2 rounded-md'>Login</div>
+            </Link>
+            <Link to='/signup'>
+              <div className='bg-slate-500 text-white px-4 py-2 rounded-md'>Sign Up</div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
