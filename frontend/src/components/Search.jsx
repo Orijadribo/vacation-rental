5acd9bf26f1602f4a1b1044320c390cd3d8d2158
Orijadribo <danielorijadribo@gmail.com>
2024-09-05 02:32:10 +0300

    responsive search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -9,23 +9,23 @@ import { FiMinus, FiPlus } from 'react-icons/fi';
 
 const Search = () => {
   return (
-    <div className='absolute left-0 right-0 bottom-10 z-20'>
+    <div className='absolute left-0 right-0 bottom-14 z-20'>
       <div className='container max-w-screen-xl mx-auto px-4'>
-        <div className='bg-white rounded-full shadow-lg p-4 md:p-5'>
+        <div className='bg-white rounded-full shadow-lg p-4 md:p-5 w-fit mx-auto'>
           <div className='flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6'>
-            <div className='flex items-center justify-center gap-2'>
+            <div className='hidden md:flex items-center justify-center gap-2'>
               <IoLocationOutline size={24} />
               <p className='text-sm md:text-base'>Select a location</p>
             </div>
             <div className='hidden md:block w-[2px] h-[30px] bg-gray-300'></div>
-            <div className='flex items-center justify-center gap-2 md:gap-4'>
+            <div className='hidden md:flex items-center justify-center gap-2 md:gap-4'>
               <IoCalendarOutline size={24} />
               <h1 className='text-sm md:text-base'>Check-in</h1>
               <IoArrowForwardOutline size={24} />
               <h1 className='text-sm md:text-base'>Check-out</h1>
             </div>
             <div className='hidden md:block w-[2px] h-[30px] bg-gray-300'></div>
-            <div className='flex items-center justify-center gap-2 md:gap-4'>
+            <div className='hidden md:flex items-center justify-center gap-2 md:gap-4'>
               <IoPeopleOutline size={24} />
               <p className='text-sm md:text-base'>Guests</p>
               <div className='flex items-center justify-center gap-5'>
