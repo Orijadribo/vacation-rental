5949d68b492364c9a462be8833bfdf3db38a0dc4
Orijadribo <danielorijadribo@gmail.com>
2024-09-04 03:49:54 +0300

    Added location on the banner

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { pic_1 } from '../assets';
+import { IoLocationOutline } from 'react-icons/io5';
 
 const Banner = () => {
   return (
-    <div className='relative h-screen w-full'>
+    <div className='relative h-screen w-full -z-10'>
       <img src={pic_1} alt='banner' className='w-full h-full object-cover' />
       <div className='absolute inset-0 flex items-center justify-center w-full h-full bg-black bg-opacity-30'>
         <div className='flex flex-col max-w-screen-xl mx-auto'>
@@ -16,6 +17,12 @@ const Banner = () => {
           <p className='text-[20px] text-white leading-[60px] tracking-wider'>
             Find the perfect getaway in beautiful locations.
           </p>
+          <div className='flex items-center gap-2 text-white text-sm mt-20'>
+            <IoLocationOutline />
+            <p>Muyenga TankHill</p>
+            <div className='flex items-center justify-center w-[2px] h-[20px] bg-white rounded-full'></div>
+            <div>Kampala City</div>
+          </div>
         </div>
       </div>
     </div>
