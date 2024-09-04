bd2c2d3fe7686660739a256fd9b29943724a0229
Orijadribo <danielorijadribo@gmail.com>
2024-09-05 01:03:37 +0300

    banner for other pages

--- frontend/src/pages/Properties.jsx
+++ frontend/src/pages/Properties.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { properties } from '../data';
+import { pic_1, pic_2, pic_3 } from '../assets';
 
 const Properties = () => {
   return (
-    <div>Properties</div>
-  )
-}
+    <div className='flex flex-col justify-center items-center'>
+      <div className=''>
+        <img src={pic_3} alt='pic_1' />
+      </div>
+      <div className='flex justify-center items-center'>
+        <h1>Properties</h1>
+      </div>
+      <div className='flex flex-wrap justify-center items-center'>
+        {properties.map((property) => (
+          <div key={property.id}>
+            <h2>{property.location}</h2>
+            <div className='flex justify-center items-center'>
+              <img src={property.image[1]} alt={property.location} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Properties
+export default Properties;
