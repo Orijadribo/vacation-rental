526e5123334a8b2bb91f705cef51a91b361a19c0
Orijadribo <danielorijadribo@gmail.com>
2024-09-04 21:55:14 +0300

    added max width

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -20,30 +20,52 @@ const NavBar = () => {
   }, [scrolled]);
 
   return (
-    <div className={`fixed w-full transition-colors duration-300 z-50 ${scrolled ? 'bg-white shadow-md' : 'bg-transparent'}`}>
-      <div className='flex justify-between items-center max-w-screen-xl mx-auto h-16'>
-        <div className='text-2xl font-bold'>Logo</div>
+    <div
+      className={`fixed w-full transition-colors duration-300 z-50 ${
+        scrolled ? 'bg-white shadow-md text-black' : 'bg-transparent text-white'
+      }`}
+    >
+      <div className='flex justify-between items-center max-w-screen-xl mx-auto h-16 px-10'>
+        <div className='text-2xl font-bold text-black'>Logo</div>
         <div className='flex justify-between items-center gap-10'>
           <div className='flex items-center justify-center space-x-4'>
             <Link to='/'>
-              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>Home</div>
+              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>
+                Home
+              </div>
             </Link>
             <Link to='/about'>
-              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>About Us</div>
+              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>
+                About Us
+              </div>
             </Link>
             <Link to='/properties'>
-              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>Properties</div>
+              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>
+                Properties
+              </div>
             </Link>
             <Link to='/contact'>
-              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>Contact Us</div>
+              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>
+                Contact Us
+              </div>
             </Link>
           </div>
           <div className='flex items-center justify-center space-x-4'>
             <Link to='/login'>
-              <div className='border-2 border-white hover:border-2 hover:border-slate-500 px-4 py-2 rounded-md'>Login</div>
+              <div
+                className={`border-2 ${
+                  !scrolled
+                    ? 'border-white hover:border-2 hover:border-slate-500'
+                    : 'border-slate-500 hover:border-2 hover:border-white'
+                } px-4 py-2 rounded-md`}
+              >
+                Login
+              </div>
             </Link>
             <Link to='/signup'>
-              <div className='bg-slate-500 text-white px-4 py-2 rounded-md'>Sign Up</div>
+              <div className='bg-slate-500 text-white px-4 py-2 rounded-md'>
+                Sign Up
+              </div>
             </Link>
           </div>
         </div>
