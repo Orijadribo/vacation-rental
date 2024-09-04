385489e46c1abccab97d6a634226d6c586402912
Orijadribo <danielorijadribo@gmail.com>
2024-09-04 21:56:09 +0300

    added padding to max screen and explore more option

--- frontend/src/components/Properties.jsx
+++ frontend/src/components/Properties.jsx
@@ -3,7 +3,7 @@ import { properties } from '../data';
 
 const Properties = () => {
   return (
-    <div className='mt-20 max-w-screen-xl mx-auto'>
+    <div className='mt-20 max-w-screen-xl mx-auto px-10'>
       <div className='flex flex-col items-center justify-center'>
         <div className='flex flex-col items-center justify-center gap-5 my-10 w-[500px]'>
           <h1 className='text-4xl font-bold'>A World of Possibilities</h1>
@@ -15,13 +15,20 @@ const Properties = () => {
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10'>
           {properties.slice(0, 3).map((property) => (
             <div key={property.id}>
+              <img
+                src={property.image[0]}
+                alt={property.location}
+                className='rounded-lg'
+              />
               <h2>{property.location}</h2>
               <p>{property.categories.join(', ')}</p>
               <p>{property.amenities.join(', ')}</p>
               <p>{property.price}</p>
-              <img src={property.image[0]} alt={property.location} />
             </div>
           ))}
+        </div>
+        <div className='text-2xl font-bold mt-10 p-5 px-10 border-2 rounded-lg bg-slate-200 hover:bg-slate-600 hover:text-white cursor-pointer'>
+          Explore More ...
         </div>
       </div>
     </div>
