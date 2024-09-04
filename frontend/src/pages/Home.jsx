7ea27f3f63e6abc9bb41b54241a83465805d1625
Orijadribo <danielorijadribo@gmail.com>
2024-09-05 01:03:16 +0300

    Created home banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Banner from '../components/Banner';
 import Search from '../components/Search';
 import Properties from '../components/Properties';
 import Locations from '../components/Locations';
 import Reviews from '../components/Reviews';
+import HomeBanner from '../components/HomeBanner';
 
 const Home = () => {
   return (
     <div>
-      <Banner />
+      <HomeBanner />
       <Search />
       <Properties />
       <Locations />
