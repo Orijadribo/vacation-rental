ee66b25b2d3285ec0636bd46e310dd76fe555242
Orijadribo <danielorijadribo@gmail.com>
2024-09-06 12:32:57 +0300

    added a marker on the current page

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NavBar = ({ setIsLogin }) => {
   const [scrolled, setScrolled] = useState(false);
+  const [pages, setPages] = useState('home');
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,25 +30,49 @@ const NavBar = ({ setIsLogin }) => {
         <div className='text-2xl font-bold text-black'>Logo</div>
         <div className='flex justify-between items-center gap-10'>
           <div className='flex items-center justify-center space-x-4'>
-            <Link to='/'>
-              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>
-                Home
-              </div>
+            <Link
+              to='/'
+              className='hover:bg-slate-600 hover:text-white px-4 py-2 rounded-md'
+            >
+              <div onClick={() => setPages('home')}>Home</div>
+              <div
+                className={`m-auto w-[80%] h-[2px] rounded-full ${
+                  scrolled ? 'bg-black shadow-md' : 'bg-white'
+                } ${pages === 'home' ? 'block' : 'hidden'}`}
+              ></div>
             </Link>
-            <Link to='/about'>
-              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>
-                About Us
-              </div>
+            <Link
+              to='/about'
+              className='hover:bg-slate-600 hover:text-white px-4 py-2 rounded-md'
+            >
+              <div onClick={() => setPages('aboutus')}>About Us</div>
+              <div
+                className={`m-auto w-[80%] h-[2px] rounded-full ${
+                  scrolled ? 'bg-black shadow-md' : 'bg-white'
+                } ${pages === 'aboutus' ? 'block' : 'hidden'}`}
+              ></div>
             </Link>
-            <Link to='/properties'>
-              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>
-                Properties
-              </div>
+            <Link
+              to='/properties'
+              className='hover:bg-slate-600 hover:text-white px-4 py-2 rounded-md'
+            >
+              <div onClick={() => setPages('properties')}>Properties</div>
+              <div
+                className={`m-auto w-[80%] h-[2px] rounded-full ${
+                  scrolled ? 'bg-black shadow-md' : 'bg-white'
+                } ${pages === 'properties' ? 'block' : 'hidden'}`}
+              ></div>
             </Link>
-            <Link to='/contact'>
-              <div className='hover:bg-slate-500 hover:text-white px-4 py-2 rounded-md'>
-                Contact Us
-              </div>
+            <Link
+              to='/contact'
+              className='hover:bg-slate-600 hover:text-white px-4 py-2 rounded-md'
+            >
+              <div onClick={() => setPages('contactus')}>Contact Us</div>
+              <div
+                className={`m-auto w-[80%] h-[2px] rounded-full ${
+                  scrolled ? 'bg-black shadow-md' : 'bg-white'
+                } ${pages === 'contactus' ? 'block' : 'hidden'}`}
+              ></div>
             </Link>
           </div>
           <div className='flex items-center justify-center space-x-4'>
@@ -55,18 +80,24 @@ const NavBar = ({ setIsLogin }) => {
               <div
                 className={`border-2 ${
                   !scrolled
-                    ? 'border-white hover:border-2 hover:border-slate-500'
-                    : 'border-slate-500 hover:border-2 hover:border-white'
+                    ? 'border-white hover:border-2 hover:bg-white/50 hover:text-black'
+                    : 'border-slate-500 hover:border-2 hover:bg-slate-500'
                 } px-4 py-2 rounded-md`}
-                onClick={() => setIsLogin(true)}
+                onClick={() => {
+                  setIsLogin(true);
+                  setPages('login');
+                }}
               >
                 Login
               </div>
             </Link>
             <Link to='/signup'>
               <div
-                className='bg-slate-500 text-white px-4 py-2 rounded-md'
-                onClick={() => setIsLogin(false)}
+                className='bg-slate-500 border border-slate-500 hover:bg-slate-600 hover:border-slate-600 text-white px-4 py-2 rounded-md'
+                onClick={() => {
+                  setIsLogin(false);
+                  setPages('signup');
+                }}
               >
                 Sign Up
               </div>
