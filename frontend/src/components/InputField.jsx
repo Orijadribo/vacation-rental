1f2332d421aee73a481c5baf161f60832665a7ca
Orijadribo <danielorijadribo@gmail.com>
2024-09-09 12:19:22 +0300

    removed max width

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -12,7 +12,7 @@ const InputField = ({ label, type, name, value, setValue }) => {
   const handleBlur = () => setIsFocused(false);
 
   return (
-    <div className='relative my-2 w-full max-w-md'>
+    <div className='relative my-2 w-full'>
       <input
         type={type}
         id={name}
@@ -22,13 +22,13 @@ const InputField = ({ label, type, name, value, setValue }) => {
         onChange={handleInputChange}
         onFocus={handleFocus}
         onBlur={handleBlur}
-        className='h-14 w-full border border-gray-300 rounded-md px-3 pt-5 pb-2 text-base focus:outline-none focus:ring-2 focus:ring-[#7108AA] focus:border-transparent'
+        className='h-14 w-full border border-gray-300 rounded-md px-3 pt-5 pb-2 text-base focus:outline-none focus:ring-2 focus:ring-slate-700 focus:border-transparent'
         placeholder=' '
       />
       <label
         className={`absolute left-3 transition-all duration-150 ease-out pointer-events-none ${
           isFocused || value
-            ? 'top-2 text-xs text-[#7108AA]'
+            ? 'top-2 text-xs text-slate-700'
             : 'top-1/2 -translate-y-1/2 text-base text-gray-500'
         }`}
         htmlFor={name}
